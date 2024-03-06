a0527d5a76018e53fc5ae26a7990eaed2d2bdd46
jbowens130 <158515070+jbowens130@users.noreply.github.com>
2024-03-06 15:54:58 -0500

    Libraries for IR remote, LCD screen, IO macros

---
+++ lib/IR_Settings.c
@@ -0,0 +1,12 @@
+/* This labels the setting for the IR reciever, and creates macros for the button values
+***************************************************************************************/
+#ifndef IR_SETTINGS_H_
+#define IR_SETTINGS_H_
+#endif
+
+#include "IO_Macros.h"
+
+
+
+
+
