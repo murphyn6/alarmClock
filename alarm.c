7292913b6281e398152ec99d388d527cca907c92
jbowens130 <158515070+jbowens130@users.noreply.github.com>
2024-03-06 15:55:39 -0500

    Add files via upload

---
+++ alarm.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "C:/Arduino_Code/lcd/lib/HD44780.h"
+#include "lib/src/remotes.h"
+
+
+int main(void)
+{
+	//Setup
+	LCD_Setup();
+	LCD_Clear();
+	initIR(5,0x00FF, );
+	
+	getIRCommandRec();
+	
+	print(getIRCommandRec());
+	while (1);
+	
+	return 0;
+}
