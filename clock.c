0c62a439f877d5999b4aebd138ed1cf01fc41ebf
murphyn6 <95459788+murphyn6@users.noreply.github.com>
2024-03-06 20:38:42 -0500

    Update clock.c

--- clock.c
+++ clock.c
@@ -2,13 +2,21 @@
 #include <time.h>
 #include <stdint.h>
 #include <unistd.h>
-#include "lib/hd44780.h"
+//#include "lib/hd44780.h"
 
 int main(void)
 {
-    while(true) {
+    int wholeAlarm;
+    int alarmHour;
+    int alarmMinute;
+    printf("Please enter your desired alarm time using military time\n");
+    scanf("%d",&wholeAlarm);
+    alarmMinute = wholeAlarm % 100;
+    alarmHour = wholeAlarm / 100;
+    printf("Your alarm is set for %d:%d\n", alarmHour, alarmMinute);
+    while(1) {
 	//Setup
-    LCD_Setup();
+    //LCD_Setup();
     int hour = 0;
     int minute = 0;
     // Get the current time
@@ -18,12 +26,16 @@ int main(void)
 	struct tm *local_time = localtime(&current_time);
     hour = local_time->tm_hour;
     minute = local_time->tm_min;
-
-		LCD_GotoXY(0, 0);
+    printf("Time: %d:%d\n",hour,minute);
+		/*LCD_GotoXY(0, 0);
 		LCD_PrintString("Time: ");
 		LCD_PrintInteger(hour);
         LCD_PrintString(":");
-		LCD_PrintInteger(minute);
+		LCD_PrintInteger(minute);*/
         sleep(1);
+    if(hour==alarmHour && minute==alarmMinute) {
+        printf("ALARM!!!");
+        return(0);
+    }
     }
 }
