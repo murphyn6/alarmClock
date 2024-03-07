89509942579ca020b25850c8149356fe3b65caa3
murphyn6 <95459788+murphyn6@users.noreply.github.com>
2024-03-06 20:19:04 -0500

    Update clock.c

--- clock.c
+++ clock.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "lib/hd44780.h"
 
+int main(void)
+{
+    while(true) {
+	//Setup
+    LCD_Setup();
+    int hour = 0;
+    int minute = 0;
+    // Get the current time
+    time_t current_time;
+    time(&current_time);
+
+	struct tm *local_time = localtime(&current_time);
+    hour = local_time->tm_hour;
+    minute = local_time->tm_min;
+
+		LCD_GotoXY(0, 0);
+		LCD_PrintString("Time: ");
+		LCD_PrintInteger(hour);
+        LCD_PrintString(":");
+		LCD_PrintInteger(minute);
+        sleep(1);
+    }
+}
